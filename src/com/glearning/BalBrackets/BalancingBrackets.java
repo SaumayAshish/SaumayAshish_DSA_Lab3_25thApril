2bf952ac810a5bc6863cf0094eb898f7c8d453f3
SaumayAshish <124284375+SaumayAshish@users.noreply.github.com>
2023-04-25 16:59:26 +0530

    Add files via upload

---
+++ src/com/glearning/BalBrackets/BalancingBrackets.java
@@ -0,0 +1,28 @@
+package com.glearning.BalBrackets;
+
+import java.util.*;
+
+public class BalancingBrackets {
+	
+
+    public static boolean isBalanced(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (c == ')' || c == ']' || c == '}') {
+                if (stack.empty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == ']' && top != '[') ||
+                    (c == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module iitrGLprojDue25thApril {
+}
